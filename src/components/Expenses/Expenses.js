1145c85f65b06c3f32e719ff08dd03e8e5641279
Baha Catherine Maigida <cathbaha1@gmail.com>
2022-01-28 11:43:35 +0100

    first commit

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+
+import Card from "../UI/Card";
+import './Expenses.css';
+import ExpensesFilter from './ExpensesFilter';
+import { useState } from 'react/cjs/react.development';
+import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+
+  };
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+
+  });
+
+
+
+    return (
+        <li>
+        <Card className="expenses">
+        <ExpensesFilter 
+        selected = {filteredYear}
+         onChangeFilter={filterChangeHandler}
+         />
+         <ExpensesChart expenses={filteredExpenses}/>
+      <ExpensesList items= {filteredExpenses}/>
+    </Card>
+    </li>
+
+    );
+}
+
+
+export default Expenses;
